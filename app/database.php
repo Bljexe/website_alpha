eb32d650d270d0f8f4439562adbab294c7de794f
Bryan Lucas <89219214+Bljexe@users.noreply.github.com>
2023-04-14 09:12:11 -0300

    Update database.php

--- app/database.php
+++ app/database.php
@@ -1,15 +1,13 @@
 <?php
-####################################
-# Britana CMS © 2017 - By Kalyptus #
-####################################
 
-//Base de donnée du login
+//Banco de Dados para Autenticação (Site)
 $login_port = 3306;
 $login_host = '127.0.0.1';
 $login_user = 'root';
 $login_pass = '99001493@';
 $login_name = 'auth';
-//Banco de Dados
+
+//Banco de Dados para Jogo (world)
 $game_port = 3306;
 $game_host = '127.0.0.1';
 $game_user = 'root';
